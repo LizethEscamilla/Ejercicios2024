fdf0baf21cb8bdf829ce6099fd5ef95732a5ed4d
Lizeth <lizeth.escamilla05@gmail.com>
2024-09-23 22:32:00 -0600

    ejercicio1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Por favor introduzca su nombre");
+int numeroPersonas = 12;
+
+String nombrePersona = Console.ReadLine();
+Console.WriteLine("En este salón hay {0} personas y una se llama {1}", numeroPersonas, nombrePersona);
+
+Console.ReadKey();
