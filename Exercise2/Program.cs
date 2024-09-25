d615268ececfaa207cd7ee872c82d6a790b9c5d6
Lizeth <lizeth.escamilla05@gmail.com>
2024-09-24 21:19:08 -0600

    ejercicio2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[]args)
+    {
+        for (int i = 1; i <= 99; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
