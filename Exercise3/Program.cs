50d47b3a3b9ea24f2258272f57ae139cd65e4f50
Lizeth <lizeth.escamilla05@gmail.com>
2024-09-25 21:45:39 -0600

    ejercicio3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el primer número: ");
+        int num1= Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Ingresa el segundo número: ");
+        int num2=Convert.ToInt32(Console.ReadLine());
+        
+        AbsoluteValue Absvalue =new AbsoluteValue();
+        int result =Absvalue.GetAbsolute(num1, num2);
+        
+        Console.WriteLine("El resultado es: " + result);
+    }
+}
