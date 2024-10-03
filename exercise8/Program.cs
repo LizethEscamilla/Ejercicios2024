2996ed6704b16c00abae057016244ed07236b8c0
Lizeth <lizeth.escamilla05@gmail.com>
2024-10-02 19:36:59 -0600

    ejercicio8

---
+++ exercise8/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int resultado = Power(2, 3);  
+        Console.WriteLine(resultado);
+    }
+
+    static int Power(int Numbase, int exponente)
+    {
+        int resultado = 1;  
+        for (int i = 0; i < exponente; i++)
+        {
+            resultado *= Numbase;  
+        }
+        return resultado;  
+    }
+}
+
