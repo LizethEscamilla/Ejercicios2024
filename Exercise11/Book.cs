f15a0320a34c6336ab9470ce7b150340c802bdb4
Lizeth <lizeth.escamilla05@gmail.com>
2024-10-06 19:38:39 -0600

    ejercicio11

---
+++ Exercise11/Book.cs
@@ -0,0 +1,16 @@
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"Título = {Title}, Autor = {Author}";
+    }
+}
---
+++ Exercise11/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Book[] books = new Book[2];
+
+        Console.WriteLine("\nIngresa la información de libro 1:");
+        books[0] = CreateBookFromUserInput();
+
+        Console.WriteLine("\nIngresa la información de libro 2:");
+        books[1] = CreateBookFromUserInput();
+
+        Console.WriteLine($"\n1: {books[0]}");
+        Console.WriteLine($"2: {books[1]}");
+    }
+
+    static Book CreateBookFromUserInput()
+    {
+        Console.Write("Ingresa el nombre del libro: ");
+        string title = Console.ReadLine();
+
+        Console.Write("Ingresa el autor: ");
+        string author = Console.ReadLine();
+
+        return new Book(title, author);
+    }
+}
+
